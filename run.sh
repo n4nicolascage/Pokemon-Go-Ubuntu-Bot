082b5dbf2bd7e344a4318ea0f911d583d4fa98f4
n4nicolascage <n4nicolascage@gmail.com>
2016-08-17 14:28:37 +0530

    Major Updates
    Major Updates

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+pokebotpath=$(cd "$(dirname "$0")"; pwd)
+filename=""
+if [ ! -z $1 ]; then
+filename=$1
+else
+filename="./configs/config.json"
+fi
+cd $pokebotpath
+source bin/activate
+git fetch -a
+if [ "1" == $(git branch -vv |grep -c "* dev") ] && [ $(git log --pretty=format:"%h" -1) != $(git log --pretty=format:"%h" -1 origin/dev) ]
+then
+echo "Branch dev hav an update. Run ./setup.sh -u to update."
+elif [ "1" == $(git branch -vv |grep -c "* master") ] && [ $(git log --pretty=format:"%h" -1) != $(git log --pretty=format:"%h" -1 origin/master) ]
+then 
+echo "Branch master hav an update. Run ./setup.sh -u to update."
+fi
+sleep 2
+if [ ! -f "$filename" ]; then
+echo "There's no "$filename" file. Please use ./setup.sh -c to creat one."
+fi
+while true
+do
+python pokecli.py -cf $filename
+echo `date`" Pokebot "$*" Stopped." 
+read -p "Press any button or wait 20 seconds to continue.
+" -r -s -n1 -t 20
+done
+exit 0
